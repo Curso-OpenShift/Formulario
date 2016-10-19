b4d072b7703c82c2703b3917a208537f4cff50cd
mozta <certificacion@localhost.localdomain>
2016-10-19 15:40:39 -0700

    Carpeta eliminada Miformulario

--- OverFlow/Miformulario/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/certificacion/Documentos/CursoRedhatOpenshift/Miformulario/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
